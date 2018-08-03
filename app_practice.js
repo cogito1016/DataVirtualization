4f47aa47f5bfaaa9f4535bd50d4667c7fb178110
cogito1016 <cogito1016@naver.com>
2018-08-03 10:35:06 +0900

    Repeating to write code-(html,js)

--- app_practice.js
+++ app_practice.js
@@ -1,3 +1,16 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
+
+fs.exists("./public/GoogleMapAPI.html",function(exists){
+    console.log(exists? 'Yes':'No');
+});
+
+app.listen(1234,function(){
+    console.log("1234 Connecting ....");
+});
+
+app.get('/',function(req,res){
+    res.send("Hello this is main homepage");
+});
+

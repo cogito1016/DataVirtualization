1a2efbcb5e568f509c7ab54dae3ca7d6fec599ac
cogito1016 <cogito1016@naver.com>
2018-08-13 22:09:02 +0900

    Basic DB connection (mysql)

---
+++ mysqlTest.js
@@ -0,0 +1,14 @@
+
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Mrla758412z!',
+    database : 'o2',
+    port : '3306'
+});
+
+connection.connect();
+
+

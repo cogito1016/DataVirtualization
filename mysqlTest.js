ed0c4fffb2c1cfa637b748492d09cb43c5932321
cogito1016 <cogito1016@naver.com>
2018-08-13 22:59:54 +0900

    DB connection complete! (Try to use basic query)

--- mysqlTest.js
+++ mysqlTest.js
@@ -11,4 +11,25 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+/*
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
 
+connection.end();*/
+
+var sql = 'SELECT * FROM topic';
+connection.query(sql,function(err,rows,fields){
+    if(err){
+        console.log(err);
+    }
+    else
+    {
+        console.log('rows',rows);
+        console.log('fields',fields);
+    }
+
+});
+
+connection.end();
